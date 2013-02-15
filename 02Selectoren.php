c9cb627d3dcd5f8de54247cee29803a9ee01b3ab
Susann <cpi@CPI-00044.training>
2013-02-15 15:08:21 +0100

    tag 2 selectoren

---
+++ 02Selectoren.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link href="css/style.css" rel="stylesheet" type="text/css">
+        <script src="js/jquery-1.9.1.js" type="text/javascript" ></script>
+        <script type="text/javascript" >
+            $(document).ready(function(){
+                $("#sel").css("background-color","#ccc");
+                $(".text").css("width","400px");
+                $("h1").addClass("classH1neu");
+                $("ol").css("background-color","#ccc").css("width","400px");
+                $("li:eq(1)").css("background-color","#f8f8f8").css("font-size","30px").css("width","800px");
+            });
+            
+        </script>
+        <meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+        <title>Selectoren Tag02</title>
+    </head>
+    <body>
+        <a href="index.php">Inhalt</a>
+        <h1>jQuery Tag2 Selectoren</h1>
+        <ol>
+            <li>Was sind Selectoren</li>
+            <li>Wie spreche ich diese an $("li:eq(1)") = zweites Listenlement soll hellgrau hinterlegt werden und der Font 16px</li>
+            <li>Chicke Funktionen</li>
+        </ol>
+        <div id="sel">
+            <p class="text">Selectoren: z.B. #sel => id="sell", .text => class="text", h1 => h1<br></p>
+            
+        </div>
+    </body>
+</html>
--- index.php
+++ index.php
@@ -4,46 +4,74 @@
         <link href="css/style.css" rel="stylesheet" type="text/css">
         <script src="js/jquery-1.9.1.js" type="text/javascript" ></script>
         <script type="text/javascript" >
-  
-            jQuery(document).ready(function(){
-
-               jQuery("h1").addClass("classH1");
-            });
             $(document).ready(function(){
-                $("h1").addClass("classH1neu");
-                $("h1").addClass("classH1neu");
-                $("h1").addClass("classH1neu");
-                $("h1").click(function(){
-                    //$("this").removeClass("classH1"); Selector
-                    $("h1:first").removeClass("classH1");
-                    $("h1:eq(2)").removeClass("classH1");
-                });
-                $("div h1").addClass("classH1H2");
-                $(".class").addClass("testA");
-                //$(".class:not(:selected)");
-		$("#absatz").click(function(){
-			$(this).addClass("text");	
-		});
-		$("#absatz").click(function(xy){
-			alert('x='+xy.clientX+' y='+xy.clientY);
-		});
-            });
-            
+               $("#navigation a:eq(0)").addClass("naviRot");
+               $("#navigation a:eq(1)").addClass("naviBlau");
+               $("#navigation a:eq(2)").addClass("naviGruen");
+               $("#navigation a:eq(3)").addClass("naviOrang");
+               $("#navigation a:eq(4)").addClass("naviYellow");
+               $("#navigation a:eq(5)").addClass("naviMangenta");
+               $("#navigation a").hover(function(){
+                   $(this).addClass("navOver");
+               });
+               $("#navigation a").mouseout(function(){
+                   $(this).removeClass("navOver");
+               });
+               $("#contentLeft  li:eq(0)").click(function(){
+                   $('#contentLeft  li:eq(0)').css("font-weight","bold").append($(".blabla p:eq(0)").addClass("blablaView"));
+                   
+               });
+               $("#contentLeft  li:eq(1)").click(function(){
+                   $('#contentLeft  li:eq(1)').css("font-weight","bold").append($(".blabla p:eq(0)").addClass("blablaView"));                   
+               });
+               $("#contentLeft  li:eq(2)").click(function(){
+                   $('#contentLeft  li:eq(2)').css("font-weight","bold").append($(".blabla p:eq(0)").addClass("blablaView"));                   
+               });
+        });          
         </script>
         <meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
-        <title>jQuery!</title>
+        <title>jQuery -Kurs Inhalt</title>
     </head>
     <body>
-        <div>jQuery Tag1 Einstieg<h1>Hallo im DIV</h1></div>
-        <h1>1Hallo</h1>
-        <h1>2Hallo</h1>
-        <h1>3Hallo</h1>
-	<div id="absatz">
-		Star Trek Deutsch: Sternenreise, Reise durchs All ist der ubergreifende Titel fur sechs Science-Fiction-Fernsehserien mit insgesamt 726 Episoden sowie fur elf Kinofilme, zahlreiche Romane, Computerspiele und andere Werke, deren Inhalte auf der 1966 von Gene Roddenberry geschaffenen Fernsehserie Raumschiff Enterprise basieren.
-	</div>
-        <script type="text/javascript" >
-            $("h1").addClass("test");
-        </script>
+    <div id="inhalt">
+        <div id="header">Eine Einf&uuml;hrung in das JavaScript-Framework
+            <h1>jQuerry Kurs</h1>
+        </div>
+        <div id="navigation">
+                <a href="#">Home</a><a href="#">News</a><a href="#">Inhalt</a><a href="#">Links</a><a href="#">Profil</a><a href="#">Kontakt</a>
+        </div>
+        <div id="content">
+            <div id="contentLeft">
+                <h2>Inhalt</h2>
+                <ol>
+                   <li>Einf&uuml;hrung</li>
+                   <li>Selectoren</li>
+                   <li>ToDo-Listen</li>
+                </ol>
+            </div>
+            <div id="contentRight">
+                <img src="images/Amelia-Earhart.png">
+            </div>
+        </div>
+        <div id="footer">(c) susann rieder 2013 - Kontakt</div>
+    </div>
+    <div class="blabla">
+        <p>
+            1. Was ist jQuery<br>
+            2. Wie wird es Eingebunden<br>
+            3. Erste Finger&uuml;bungen
+        </p>
+        <p>
+            1. Selectoren<br>
+            2. Beispiele<br>
+            3. Projekt
+        </p>
+        <p>
+            1. Einarbeitung NetBeans<br>
+            2. GitHub<br>
+            3. kleines Tutorial erstellen
+        </p>
+    </div>
     </body>
 </html>
 
