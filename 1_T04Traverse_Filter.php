aa09843570bf0398fb595ed214e5931f6394dc54
Susann <cpi@CPI-00044.training>
2013-02-19 11:50:57 +0100

    Tag 4 zwischenstand, neue dateinamen 1_T01 datei 1 von tag 01, Traverse mit siblings

--- 1_T04Traverse_Filter.php
+++ 1_T04Traverse_Filter.php
@@ -26,6 +26,10 @@
                 //childrin
                 $(".normal").children().css("border","2px solid red");
                 $(this).parent().css({border:'2px solid green'});//Objektschreibweise
+                $(".normal").children("h1").css({color:'blue'});
+              //  $(this).next("h2").css({background:'#f8f8f8'});//h2 hat keine Auswirkungen?
+                $(this).nextAll("h2").css({background:'#f8f8f8'});
+                $(this).prev().css({background:'yellow'});
                });
             });
             
@@ -35,16 +39,22 @@
     </head>
     <body>
         <a href="index.php">Inhalt</a>
-        <h1>jQuery Tag3 ansprechen von Übergeordeneten Strukturen</h1>
+        <h1>jQuery Tag3 ansprechen von &Uuml;bergeordeneten Strukturen</h1>
         Aufgabe: in einem div ist ein p, das p wird geklick und damit soll sich auch das div ver&auml;ndern
         <div class="normal">
             <p class="text">ver&auml;ndern des Elternelemnts mit parent</p>
+            <h2>Eine h2 &Uuml;berschrift</h2>
         </div>
         <div class="normal">
             <p class="text">Die ist ein zweiter div un p container</p>
+            <h1>Eine &Uuml;berschrift</h1>
+            <h2>Eine h2 &Uuml;berschrift</h2>
         </div>
         <div class="normal">
-            <p class="text">Traversierung:Transport von einer Stelle zu einer anderen, Bewegung im DOM, Traverse über Kind- oder Elternknoten </p>
+            <h2>davor Eine h2 &Uuml;berschrift</h2>
+            <p class="text">Traversierung:Transport von einer Stelle zu einer anderen, Bewegung im DOM, 
+                Traverse über Kind- oder Elternknoten </p>
+            <h2>danach Eine h2 &Uuml;berschrift</h2>
         </div>
     </body>
 </html>
