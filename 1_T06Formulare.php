d8e1e438821178d30861a55209bf27fe27938d06
Susann <cpi@CPI-00044.training>
2013-02-21 14:02:07 +0100

    Zwischenstand, formulare

--- 1_T06Formulare.php
+++ 1_T06Formulare.php
@@ -4,69 +4,47 @@
        <meta charset="utf-8">
        <style type="text/css"> 
            body,html {height:100%;width:100%}
-            #rechteMaus {
-                display: none;
-                position: absolute;
-                background-color: gold;
-            }
-            ul {
-                list-style-type: none;
-                margin: 0;
-                padding: 0;
-                padding: 5px;
-            }
         </style>
         <script src="js/jquery-1.9.1.js" type="text/javascript" ></script>
         <script type="text/javascript" >
            
     $(document).ready(function(){
-     $(this).data("name","Earhart");
-     $(this).data({
-         vname:"Amalia",job:"Pilot"
-     });
-     $(this).data({
-         person:
-          [
-           {
-            name:"Earhart",
-            vname:"Amalia",
-            job:"Pilot" 
-           },
-           {
-            name:"Rieder",
-            vname:"Susann",
-            job:"Paddler" 
-           },
-         ]
-     });
-     var p = $(this).data().person;
-     $("p").eq(2).text($(this).data().person[0].name+" "+$(this).data().person[0].job);
-     console.log($(this).data());
-     console.log($(this).data().job);
-     console.log($(this).data().person[0].name);
-     console.log(p);
+        $("#aus").click(function(){
+            if($("#i1").is(':enabled')){
+              $("#i1").attr('disabled','disabled');  
+            }
+        });
+       
+        $("#c1").click(function(){
+           $('input[type=checkbox]').attr('checked','checked');
+        });
+        $('input#i1').change(function(){
+            this.value = this.value+"kkk";
+        });
+        console.log($('#form1 :input[type=checkbox]').length);
+        console.log($('#form1 :input[type=text]').length);
+        console.log($('#form1 :input').length);
     });
 
         </script>
         <meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
         <title>Events Tag06, Data</title>
     </head>
-    <body oncontextmenu="return false">
-        <a href="index.php">Inhalt</a>
-        <h1>jQuery Tag6 Data</h1>
-        <div id="normal">
-            <p id="test">Funktioniert nur im Body: height und wight setzen</p>
-            <p>Bitte rechts Klicken</p>
-            <p>Links klicken Menü verstecken</p>
-            <p>überall klicken</p>
-        </div>
-        <nav>
-            <ul>
-                <li>Kanu</li>
-                <li><a href="#" title="paddel">Paddel</a></li>
-                <li><a href="http://www.globetrotter.de">Schwimmweste</a></li>
-            </ul>
-        </nav>
-   <img src="images/Amelia-Earhart.png">
+    <body>
+        <div id="box1">
+<form id="form1" action="#">
+<p>Geben Sie hier einen Text mit führenden und/oder folgenden Leerzeichen ein:</p>
+<input type="text" name="eingabe1" id="i1" value="test"/><br>
+<input type="text" name="eingabe2" id="i2" /><br>
+
+<input type="button" id="aus" value="aus"><input type="button" id="an" value="an"><br>
+<input type="checkbox" name="eingabe" id="c1" />alle<br>
+<input type="checkbox" name="eingabe" id="c2" />b<br>
+<input type="checkbox" name="eingabe" id="c3" />c<br>
+<input type="checkbox" name="eingabe" id="c4" />d<br>
+<input type="checkbox" name="eingabe" id="c5" />e<br>
+</form>
+</div>
+
     </body>
 </html>
